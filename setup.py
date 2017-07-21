0bc3629490bdf5b3b5281f5e1a4d3cc612514046
Victor Porton <porton@narod.ru>
2017-07-21 22:20:23 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-dump-http',
+    version='0.0.1',
+    packages=['dump_http'],
+    url='https://github.com/vporton/django-dump-http',
+    license='MIT',
+    author='Victor Porton',
+    author_email='porton@narod.ru',
+    description='Dump all Django HTTP requests and responses into files'
+)
